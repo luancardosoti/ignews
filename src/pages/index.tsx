9b3f5732f4570a4232d1733f3c783e77f0a038b7
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2021-12-07 12:07:53 -0300

    titulo dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from 'next/head';
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Olá Página
-    </h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1 className={styles.title}>
+        Olá <span> Página </span>
+      </h1>
+    </>
   )
 }
