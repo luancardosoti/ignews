f5510d184c16c14d6c7082464b8a6174a69db0f5
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2021-12-23 00:07:57 -0300

    erro ao criar evento

---
+++ src/pages/api/webhooks.ts
@@ -0,0 +1,53 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream'
+import Stripe from 'stripe';
+import { stripe } from '../../services/stripe';
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === 'string' ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    borderParser: false
+  }
+}
+
+const relevantEvents = new Set([
+  'checkout.session.completed'
+])
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'POST') {
+    const buf = await buffer(req);
+    const secret = req.headers['stripe-signature'];
+
+    let event: Stripe.Event;
+
+    try {
+      event = stripe.webhooks.constructEvent(buf, secret, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (error) {
+      console.log(`Webhook error: ${error.message}`);
+      return res.status(400).send(`Webhook error: ${error.message}`)
+    }
+
+    const { type } = event
+
+    if (relevantEvents.has(type)) {
+      console.log('evento recebido', event)
+    }
+
+    return res.status(200).json({ received: true })
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not Allowed');
+  }
+}
