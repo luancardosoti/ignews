5d82bbe6a8cf064e1f2de1744d8137d406b7d402
Luan Cardoso <72113891+luancardosoti@users.noreply.github.com>
2021-12-23 22:17:05 -0300

    listando posts em tela

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,12 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic';
 
 import styles from './styles.module.scss';
 
-export default function Post() {
+type Post = {
+  slug: string;
+  title: string;
+  excert: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Post({ posts }: PostProps) {
   return (
     <>
       <Head>
@@ -15,21 +27,13 @@ export default function Post() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis minus necessitatibus, quaerat fugit corrupti illum deleniti non architecto, blanditiis impedit vel aliquam dicta, modi ducimus facilis! Porro saepe voluptatibus reiciendis!</p>
-          </a>
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis minus necessitatibus, quaerat fugit corrupti illum deleniti non architecto, blanditiis impedit vel aliquam dicta, modi ducimus facilis! Porro saepe voluptatibus reiciendis!</p>
-          </a>
-          <a href="#">
-            <time>12 de Março de 2021</time>
-            <strong>Mapas com React usando Leaflet</strong>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corporis minus necessitatibus, quaerat fugit corrupti illum deleniti non architecto, blanditiis impedit vel aliquam dicta, modi ducimus facilis! Porro saepe voluptatibus reiciendis!</p>
-          </a>
+          {posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excert}</p>
+            </a>
+          ))}
         </div>
       </main>
     </>
@@ -46,9 +50,21 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100
   })
 
-  console.log(response);
+  const posts = response.results.map(post => {
+
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excert: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-br', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: { posts }
   }
 }
